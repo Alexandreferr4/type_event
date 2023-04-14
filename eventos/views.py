c676fb501ffb59cc990efa05ab3f4341358ca191
Alexandreferr4 <alexandreferr4@gmail.com>
2023-04-14 11:28:27 -0300

    gerenciar eventos

--- eventos/views.py
+++ eventos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Evento
@@ -40,3 +40,18 @@ def novo_evento(request):
         
         messages.add_message(request, constants.SUCCESS, f'Evento "{nome}" cadastrado com sucesso')
         return redirect(reverse('novo_evento'))
+@login_required    
+def gerenciar_evento(request):
+    if request.method == "GET":
+        nome = request.GET.get('nome')
+        eventos = Evento.objects.filter(criador=request.user)
+        #TO DO: Realizar outros filtros
+
+        if nome:
+            eventos = eventos.filter(nome__contains=nome)
+        return render(request, 'gerenciar_evento.html', {'eventos': eventos})
+@login_required   
+def inscrever_evento(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if request.method == "GET":
+        return render(request, 'inscrever_evento.html', {'evento': evento})
